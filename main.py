299e8f29dab4941de9c0b875e230732c36cb31e4
Wiktor <wiktor.zycho99@gmail.com>
2021-04-12 00:27:45 +0200

    For safety reasons

---
+++ main.py
@@ -0,0 +1,16 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from view.MainPage import *
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = MainPage(MainWindow)
+    
+    
+    MainWindow.show()
+
+    sys.exit(app.exec_())
